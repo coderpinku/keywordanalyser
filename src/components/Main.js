0ddb5b468e669f8805caecc6252a6a4c7b108601
Pinku <coderpinku@gmail.com>
2023-03-29 11:38:34 +0530

    second commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,12 +12,13 @@ const [showdata, setShowData] = useState(false);
         <h1 className="text-4xl mb-4 text-center font-semibold text-[#495057]"> Find Great Keywords Using Google Autocomplete </h1>
       </div>
       <div>
-        <ul className='flex max-md:flex-wrap justify-center gap-2  items-center'>
+        <ul className='flex max-md:flex-wrap justify-center gap-2  items-center max-md:[&>:not(:first-child)]:hidden max-md:justify-start max-md:pl-2'>
           <li className='w-27 pl-[20px] py-1 font-semibold border-b-[3px] border-[#6610f2]'>
             <a href='#'>
               <img className='h-6 w-6 inline mr-1' src='./img/google.png' />
               <span>
                 Google
+                <AiFillCaretDown className='max-md:inline hidden max-md:block' />
               </span>
 
             </a>
@@ -68,21 +69,21 @@ const [showdata, setShowData] = useState(false);
       <div className='flex justify-center items-center mt-4 max-md:flex-wrap'>
 
         <div className='flex max-md:flex-[1_1_20rem]'>
-          <div className='h-[2.3rem] pt-1 border-black border-[1px] border-r-[0px] rounded-l-full text-[#495057] '>
+          <div className='h-[2.3rem] pt-1 border-[#495057] border-[1px] border-r-[0px] rounded-l-full text-[#495057] '>
             <span className='cursor-pointer pl-2 pr-1'>All <AiFillCaretDown className='inline' /></span>
          </div>
-          <input type="text" placeholder='type a keyword and press enter' className='max-md:flex-[1_1_20rem] h-[2.3rem] border-black border-[1px] border-r-[0px] pl-2 focus:outline-none focus:border-[#6610f2] max-md:border-r-[1px] max-md:rounded-r-full' />
+          <input type="text" placeholder='type a keyword and press enter' className='max-md:flex-[1_1_20rem] h-[2.3rem] border-[#495057] border-[1px] border-r-[0px] px-2 focus:outline-none focus:border-[#6610f2] max-md:border-r-[1px] max-md:rounded-r-full' />
     </div>
-          <input type="text" placeholder='Global / Worldwide (All Countries)' className='max-md:flex-[1_1_20rem] w-[18rem] h-[2.3rem] border-black border-[1px] pl-2 max-md:rounded-full border-r-[0px] focus:outline-none max-md:rounded-l-full max-md:mt-4' />
-          <select className='max-md:flex-[1_1_20rem] w-[8rem] h-[2.3rem] max-md:rounded-full focus:outline-none border-black border-[1px] max-md:mt-4 border-r-[0px] text-[#495057] cursor-pointer'>
+          <input type="text" placeholder='Global / Worldwide (All Countries)' className='max-md:flex-[1_1_20rem] w-[18rem] h-[2.3rem] border-[#495057] border-[1px] pl-2 max-md:rounded-full border-r-[0px] focus:outline-none max-md:rounded-l-full max-md:mt-4' />
+          <select className='max-md:flex-[1_1_20rem] w-[8rem] h-[2.3rem] max-md:rounded-full focus:outline-none border-[#495057] border-[1px] max-md:mt-4 border-r-[0px] text-[#495057] cursor-pointer'>
             <option>English</option>
             <option>Hindi</option>
             <option>Malyalam</option>
             <option>French</option>
           </select>
 
-       <div className='h-[2.3rem] ml-0 pr-2 pt-1 border-[#6610f2] border-[2px] bg-[#6610f2] max-md:flex-[1_1_20rem] text-white rounded-r-full max-md:hidden'>
-          <button className='cursor-pointer' onClick={searchData}><AiOutlineSearch size={18} className='inline mx-4' /></button>
+       <div className='h-[2.3rem] ml-0 pr-2 pt-1 border-[#6610f2] border-[2px] bg-[#6610f2] hover:contrast-200 max-md:flex-[1_1_20rem] text-white rounded-r-full max-md:hidden'>
+          <button className='cursor-pointer ' onClick={searchData}><AiOutlineSearch size={18} className='inline mx-4 ' /></button>
         </div>
 
         <div className='hidden max-md:block h-[2.3rem] w-[40rem] text-center mt-3 ml-0 pr-2 pt-1 border-[#6610f2] border-[1px] bg-[#6610f2] text-white rounded-full'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,11 +39,11 @@ const Navbar = () => {
         
         <nav className='flex justify-between items-center p-4 border-b-[1.5px]'>
             <div>
-                <h1 className='text-3xl font-bold text-[#495057]'>Keyword Analyser</h1>
+                <h1 className='text-3xl font-bold'>Keyword Analyser</h1>
             </div>
             <div className='max-md:hidden'>
-                <ul className='flex gap-6'>
-                    <li onClick={toggleMenu} className='cursor-pointer' >
+                <ul className='flex gap-6 text-[#495057]'>
+                    <li onClick={toggleMenu} className='cursor-pointer hover:text-black' >
 
                         Find Keywords <AiFillCaretDown className='inline' />
 
@@ -114,7 +114,7 @@ const Navbar = () => {
                         </ul>
                     </li>
 
-                    <li onClick={toggleMenu2}>
+                    <li onClick={toggleMenu2} className='cursor-pointer hover:text-black'>
                         Check Search Volume <AiFillCaretDown className='inline' />
                         <ul className=' absolute w-40 h-60 border-2 pt-2 top-14 rounded-md shadow bg-white' style={{ display: show1 ? "block" : "none" }}>
                             <li className='bg-[#6610f2] text-white  w-27 pl-[20px] py-1 font-semibold'>
@@ -182,7 +182,7 @@ const Navbar = () => {
                             </li>
                         </ul>
                     </li>
-                    <li onClick={toggleMenu3}>
+                    <li onClick={toggleMenu3} className='cursor-pointer hover:text-black'>
                         Keyword tool <AiFillCaretDown className='inline' />
                         <ul class="absolute w-[15rem] h-48  border-2 py-2 ml-4 mt-3 flex flex-col gap-1 rounded-md shadow bg-white" style={{ display: show2 ? "flex" : "none" }}>
                             <li className='w-full pl-4 hover:text-white hover:bg-[#6610f2]'>
@@ -205,7 +205,7 @@ const Navbar = () => {
                             </li>
                         </ul>
                     </li>
-                    <li onClick={toggleMenu4}>
+                    <li onClick={toggleMenu4} className='cursor-pointer hover:text-black'>
                         API Access <AiFillCaretDown className='inline' />
                         <ul class="absolute w-[12rem] h-20  border-2 py-2 ml-4 mt-3 flex flex-col gap-1 rounded-md shadow bg-white" style={{ display: show3 ? "block" : "none" }}>
                             <li className='w-full pl-4 hover:text-white hover:bg-[#6610f2]'>
@@ -220,9 +220,9 @@ const Navbar = () => {
             </div>
             <div className='max-md:hidden'>
                 <ul className='flex gap-6 '>
-                    <li><a href='#' className='text-[#6610f2]'><BsFillEnvelopeHeartFill className='inline mb-1' /> Contact</a></li>
-                    <li><a href='#' className='text-[#6610f2]'><BsFillCartDashFill className='inline mb-1' /> Keyword</a></li>
-                    <li><a href='#' className='bg-[#6610f2] px-4 py-2 rounded-full text-white'><BsFillPeopleFill className='inline mb-1' /> Login</a></li>
+                    <li><a href='#' className='text-[#6610f2] hover:contrast-200'><BsFillEnvelopeHeartFill className='inline mb-1' /> Contact</a></li>
+                    <li><a href='#' className='text-[#6610f2] hover:contrast-200'><BsFillCartDashFill className='inline mb-1' /> Keyword</a></li>
+                    <li><a href='#' className='bg-[#6610f2] hover:contrast-200 px-4 py-2 rounded-full text-white'><BsFillPeopleFill className='inline mb-1' /> Login</a></li>
                 </ul>
             </div>
             <div className='hidden max-md:block' onClick={() => setMenuBar(!menuBar)}>
@@ -235,7 +235,7 @@ const Navbar = () => {
                 }
             </div>
                 </nav>
-            <div className={menuBar ? 'flex flex-col top-0 left-0 bg-slate-400 h-screen w-[80%] absolute p-4 border-[1.5px] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+            <div className={menuBar ? 'flex flex-col top-0 left-0 bg-white h-screen w-[80%] absolute p-4 border-[1.5px] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
             <div>
                 <h1 className='text-2xl font-bold'>Keyword Analyser</h1>
             </div>
